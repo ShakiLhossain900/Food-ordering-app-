5d89c61496d6959ff36d4a1521024613f3ba41c7
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2022-06-11 00:29:52 +0600

    adding something me

--- src/components/Meals/MealIteam/MealIteam.js
+++ src/components/Meals/MealIteam/MealIteam.js
@@ -5,15 +5,15 @@ import clasess from "./MealItem.module.css";
 const MealIteam = (props) => {
   const price = `$ ${props.price.toFixed(2)} `;
   return (
-    <li>
-      <div className={clasess.meal}>
+    <li className={clasess.meal}>
+      <div >
         <h3>{props.name}</h3>
         <div className={clasess.description}>{props.description}</div>
         <div className={clasess.price}>{price}</div>
+      </div>
         <div>
           <MealIteamForm />
         </div>
-      </div>
     </li>
   );
 };
--- src/components/Meals/MealIteam/MealIteamForm.js
+++ src/components/Meals/MealIteam/MealIteamForm.js
@@ -1,9 +1,17 @@
+import Input from '../../UI/Input';
 import classes from './MealItemForm.module.css'
 const MealIteamForm = (props) => {
     return (
       <form className={classes.form}  >
-          <input />
-          <button>+ add</button>
+          <Input label="amount" input={{
+            id:'amount',
+            type:'number',
+            min:'1',
+            max:'5',
+            step:'1',
+            defaultValue:'1'
+          }}/>
+          <button> + add</button>
       </form>    
     );
 
