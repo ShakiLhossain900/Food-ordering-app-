49d2cc8404d8db931e752081f1de4fecae41fef0
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2022-06-10 19:27:09 +0600

    adding something

--- src/components/Meals/MealIteam/MealIteam.js
+++ src/components/Meals/MealIteam/MealIteam.js
@@ -1,21 +1,21 @@
-import React from 'react';
-import MealIteamForm from '../MealIteam/MealIteamForm';
-import clasess from './MealItem.module.css';
+import React from "react";
+import MealIteamForm from "../MealIteam/MealIteamForm";
+import clasess from "./MealItem.module.css";
 
 const MealIteam = (props) => {
-    const price = `$ ${props.price.toFixed(2)} `
-    return (
-        <li>
-            <div className={clasess.meal}>
-                <h3>{props.name}</h3>
-                <div className={clasess.description}>{props.description}</div>
-                <div className={clasess.price}>{price}</div>
-            </div>
-            <div>
-               <MealIteamForm/>
-            </div>
-        </li>
-    );
+  const price = `$ ${props.price.toFixed(2)} `;
+  return (
+    <li>
+      <div className={clasess.meal}>
+        <h3>{props.name}</h3>
+        <div className={clasess.description}>{props.description}</div>
+        <div className={clasess.price}>{price}</div>
+        <div>
+          <MealIteamForm />
+        </div>
+      </div>
+    </li>
+  );
 };
 
 export default MealIteam;
