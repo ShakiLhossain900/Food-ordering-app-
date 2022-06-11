7b580a43dc08322773593d7a16476174ae8c3887
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2022-06-11 19:12:53 +0600

    adding something new

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
- import { Fragment } from "react";
+ import { Fragment,useState } from "react";
 import Cart from "./components/Cart/Cart";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 function App() {
+  const [cartShown, setcartShown] = useState(false);
+
+  const showCartHandler =()=>{
+    setcartShown(true);
+  }
+  
+  const hideCartHandler =()=>{
+    setcartShown(false);
+  }
   return (
     <Fragment>
-      <Cart/>
-      <Header />
+     { cartShown && <Cart onClose ={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -20,8 +20,8 @@ const Cart = (props) => {
         <span>35.62</span>
       </div>
       <div className={classes.actions}>
-        <button>Close</button>
-        <button>Order</button>
+        <button className={classes['button-alt']} onClick={props.onClose} >Close</button>
+        <button className={classes.button}>Order</button>
       </div>
     </Modal>
   );
