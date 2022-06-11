41a734e04edc54d460c3984db7802700516489ec
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2022-06-11 08:43:23 +0600

    adding something new

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Cart = (props) => {
+  const cartTtems = (
+    <ul>
+      {[{ id: "c1", name: "Sushi", amount: 2, price: 12.99 }].map((item) => (
+        <li>{item.name}</li>
+      ))}
+    </ul>
+  );
+  return (
+    <div>
+      {cartTtems}
+      <div>
+        <span>Total Amount</span>
+        <span>35.62</span>
+      </div>
+      <div>
+        <button>Close</button>
+        <button>Order</button>
+      </div>
+    </div>
+  );
+};
+
+export default Cart;
