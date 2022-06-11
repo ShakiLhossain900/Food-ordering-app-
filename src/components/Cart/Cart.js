cd2da8cf4bf5f61a330310b0f37e82b5f9233f5f
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2022-06-11 11:11:32 +0600

    adding something

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,25 +1,29 @@
 import React from "react";
+import Modal from "../UI/Modal";
+
+import classes from './Cart.module.css'
 
 const Cart = (props) => {
   const cartTtems = (
-    <ul>
+    // ai class ta array vitor use kroar karon of das
+    <ul className={classes['cart-items']}>  
       {[{ id: "c1", name: "Sushi", amount: 2, price: 12.99 }].map((item) => (
         <li>{item.name}</li>
       ))}
     </ul>
   );
   return (
-    <div>
+    <Modal>
       {cartTtems}
-      <div>
+      <div className={classes.total}>
         <span>Total Amount</span>
         <span>35.62</span>
       </div>
-      <div>
+      <div className={classes.action}>
         <button>Close</button>
         <button>Order</button>
       </div>
-    </div>
+    </Modal>
   );
 };
 
