d721d9f0e89acae497b31bfa05995a84f6dd6968
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2022-06-13 16:54:59 +0600

    adding

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
- import { Fragment,useState } from "react";
+ import { useState } from "react";
 import Cart from "./components/Cart/Cart";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
+import CartProvider from "./store/CartProvider";
 function App() {
   const [cartShown, setcartShown] = useState(false);
 
@@ -13,13 +14,13 @@ function App() {
     setcartShown(false);
   }
   return (
-    <Fragment>
+    <CartProvider>
      { cartShown && <Cart onClose ={hideCartHandler} />}
       <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
-    </Fragment>
+    </CartProvider>
   );
 }
 
