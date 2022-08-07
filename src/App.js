bbd79d8666af331f5eabdf03f875443082a89954
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2022-08-07 22:25:57 +0600

    sde

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
  //than need to do recheck module 17 introduction agian and start code with this
  //exam is going on thats why the code is not running on
  //after finish the exam in 14 i will be do unlimitated code inshaallah
+ // after done module 17 i will start module 18 
  import { useState } from "react";
 import Cart from "./components/Cart/Cart";
 import Header from "./components/Layout/Header";
