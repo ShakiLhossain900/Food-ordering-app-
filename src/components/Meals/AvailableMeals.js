1d8e11f3424509053f2d5c9aaec4c91efc707ac0
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2022-08-12 07:47:00 +0600

    fetch

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -2,33 +2,36 @@ import React from "react";
 import Card from "../UI/Card";
 import MealIteam from "./MealIteam/MealIteam";
 import classes from "./AvailableMeals.module.css";
-const DUMMY_MEALS = [
-  //manual 4 data alrady created in firebase
-  {
-    id: "m1",
-    name: "Sushi",
-    description: "Finest fish and veggies",
-    price: 22.99,
-  },
-  {
-    id: "m2",
-    name: "Schnitzel",
-    description: "A german specialty!",
-    price: 16.5,
-  },
-  {
-    id: "m3",
-    name: "Barbecue Burger",
-    description: "American, raw, meaty",
-    price: 12.99,
-  },
-  {
-    id: "m4",
-    name: "Green Bowl",
-    description: "Healthy...and green...",
-    price: 18.99,
-  },
-];
+
+
+//comment out the dummey meal data now time to use Http data
+// const DUMMY_MEALS = [
+//   //manual 4 data alrady created in firebase
+//   {
+//     id: "m1",
+//     name: "Sushi",
+//     description: "Finest fish and veggies",
+//     price: 22.99,
+//   },
+//   {
+//     id: "m2",
+//     name: "Schnitzel",
+//     description: "A german specialty!",
+//     price: 16.5,
+//   },
+//   {
+//     id: "m3",
+//     name: "Barbecue Burger",
+//     description: "American, raw, meaty",
+//     price: 12.99,
+//   },
+//   {
+//     id: "m4",
+//     name: "Green Bowl",
+//     description: "Healthy...and green...",
+//     price: 18.99,
+//   },
+// ];
 
 const AvailableMeals = () => {
   const mealsList = DUMMY_MEALS.map((meal) => (
