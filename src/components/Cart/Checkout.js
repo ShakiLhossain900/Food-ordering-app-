d56216ddadb1a9aa890582c4bb2c168f5dde5a74
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2022-08-22 20:12:00 +0600

    adding something

---
+++ src/components/Cart/Checkout.js
@@ -0,0 +1,10 @@
+import classes from './Checkout.module.css';
+
+const Checkout = props => {
+    return <form action="">
+        <div className={classes.control}>
+            <label htmlFor=""></label>
+        </div>
+    </form>
+}
+export default Checkout;
