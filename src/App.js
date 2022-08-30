7804d29595c1863b3f70540189acfffd0f2bfd55
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2022-08-30 23:10:46 +0600

    adding

--- src/App.js
+++ src/App.js
@@ -1,7 +1,3 @@
-//the code day start with allah 
-//inshaallah
-//allah the code start with your name allah time manage kore dao
-//allah always great planer
 
  import { useState } from "react";
 import Cart from "./components/Cart/Cart";
